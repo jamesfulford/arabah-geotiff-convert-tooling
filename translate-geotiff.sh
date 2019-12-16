9b1cf8f3287920d065b80171cfeae479ce935c70
James Fulford <james.fulford@outlook.com>
2019-12-16 10:55:44 -0500

    feat(translate-geotiff): Clip by mask layer

--- translate-geotiff.sh
+++ translate-geotiff.sh
@@ -2,5 +2,21 @@
 
 for l in ./lists/*.vrt; do
     echo $l
-  gdal_translate -of "GTiff" "$l" "$l.tiff"
+  gdal_translate -of "GTiff" "$l" "$l-tmp.tif"
+
+  # Cut to shape
+  gdalwarp \
+    -of GTiff \
+    -cutline ./shapes/biblical-arabah.shp \
+    -cl biblical-arabah \
+    -crop_to_cutline \
+    "$l-tmp.tif" \
+    "$l.biblical.tiff"
+  gdalwarp \
+    -of GTiff \
+    -cutline ./shapes/modern-arabah.shp \
+    -cl modern-arabah \
+    -crop_to_cutline \
+    "$l-tmp.tif" \
+    "$l.modern.tiff"
 done
