afb2e141f4d7f456026d0299394dedc2310f04fb
James Fulford <james.fulford@outlook.com>
2019-12-16 23:32:40 -0500

    feat(qgis): Additive and Multiplicative diffs

--- qgis/layers_script.py
+++ qgis/layers_script.py
@@ -8,9 +8,19 @@ import os
 
 all_layers = [tree_layer.layer() for tree_layer in QgsProject.instance().layerTreeRoot().findLayers()]
 all_ndvi_layers = sorted(list(filter(lambda l: "ndvi" in l.name(), all_layers)), key=lambda l: l.name())
-output_directory = "/Users/jamesfulford/Desktop/Harvard Remote Sensing/arabah-project/diffs"
+output_directory = "/Users/jamesfulford/Desktop/Harvard Remote Sensing/Final/arabah-project/diffs"
 try:
     os.mkdir(output_directory)
+except OSError:
+    pass
+
+try:
+    os.mkdir(output_directory + "/additive")
+except OSError:
+    pass
+
+try:
+    os.mkdir(output_directory + "/multiplicative")
 except OSError:
     pass
 
@@ -23,21 +33,47 @@ def buildCalculatorEntry(layer):
     return raster
 
 
+additive_calculation = '{to_name}@1 - {from_name}@1'
+add_format = "{output_directory}/additive/{from_name}-{to_name}.tiff"
+
+multiplicative_calculation = '{to_name}@1 / {from_name}@1'
+mult_format = "{output_directory}/multiplicative/{from_name}-{to_name}.tiff"
+
+
 def calculate_diff(from_layer, to_layer):
     from_raster = buildCalculatorEntry(from_layer)
     to_raster = buildCalculatorEntry(to_layer)
-    output_path = "{output_directory}/{from_name}-{to_name}.tiff".format(
+    # Additive calculation
+    output_path_additive = add_format.format(
         output_directory=output_directory,
         from_name=from_layer.name(),
         to_name=to_layer.name()
     )
-    print(output_path)
     calc = QgsRasterCalculator(
-        '{to_name}@1 - {from_name}@1'.format(
+        additive_calculation.format(
             from_name=from_layer.name(),
             to_name=to_layer.name()
         ),
-        output_path,
+        output_path_additive,
+        'GTiff',
+        to_layer.extent(),
+        to_layer.width(),
+        to_layer.height(),
+        [from_raster, to_raster]
+    )
+    calc.processCalculation()
+    # Multiplicative calculation
+    output_path_multiplicative = mult_format.format(
+        output_directory=output_directory,
+        from_name=from_layer.name(),
+        to_name=to_layer.name()
+    )
+    calc = QgsRasterCalculator(
+        multiplicative_calculation.format(
+            from_name=from_layer.name(),
+            to_name=to_layer.name()
+        ),
+        output_path_multiplicative,
         'GTiff',
         to_layer.extent(),
         to_layer.width(),
@@ -55,4 +91,5 @@ for shape in ["biblical", "modern"]:
         from_layer = ndvi_layers[from_index]
         to_layer = ndvi_layers[to_index]
         calculate_diff(from_layer, to_layer)
+    # total
     calculate_diff(ndvi_layers[0], ndvi_layers[-1])
